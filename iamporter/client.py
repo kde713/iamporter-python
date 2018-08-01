98cdc20e8f6f3c4c93fa146ce1c378f794faf6d9
kde713 <kde713@gmail.com>
2018-08-02 08:28:35 +0900

    Iamporter Class 업데이트
    - cancel_payment 추가
    - find를 find_payment로 메소드 이름 변경
    - wildcard import 제거

--- iamporter/client.py
+++ iamporter/client.py
@@ -1,9 +1,9 @@
 from requests import Session
 from requests.adapters import HTTPAdapter
 
-from .base import *
+from .base import IamportAuth
 from .errors import ImpUnAuthorized, ImpApiError
-from .api import *
+from .api import Payments
 
 __all__ = ['Iamporter']
 
@@ -37,12 +37,12 @@ class Iamporter:
         requests_adapter = HTTPAdapter(max_retries=3)
         self.requests_session.mount('https://', requests_adapter)
 
-    def find(self, imp_uid=None, merchant_uid=None):
+    def find_payment(self, imp_uid=None, merchant_uid=None):
         """아임포트 고유번호 또는 가맹점지정 고유번호로 결제내역을 확인합니다
 
         Args:
             imp_uid (str): 아임포트 고유번호
-            merchant_uid (str): 결제요청 시 가맹점에서 요청한 merchant_uid
+            merchant_uid (str): 결제요청 시 가맹점에서 요청한 merchant_uid. imp_uid와 merchant_uid 중 하나는 필수어야합니다. 두 값이 모두 넘어오면 imp_uid를 우선 적용합니다.
 
         Returns:
             dict
@@ -54,6 +54,29 @@ class Iamporter:
             response = api_instance.get_find(merchant_uid)
         else:
             raise KeyError('imp_uid와 merchant_uid 중 하나를 반드시 지정해야합니다.')
+
+        if not response.is_succeed:
+            raise ImpApiError(response)
+
+        return response.data
+
+    def cancel_payment(self, imp_uid=None, merchant_uid=None, amount=None, tax_free=None, reason=None):
+        """승인된 결제를 취소합니다.
+
+        Args:
+            imp_uid (str): 아임포트 고유 번호
+            merchant_uid (str): 가맹점지정 고유 번호. imp_uid와 merchant_uid 중 하나는 필수이어야합니다. 두 값이 모두 넘어오면 imp_uid를 우선 적용합니다.
+            amount (float): 취소 요청 금액. 누락 시 전액을 취소합니다.
+            tax_free (float): 취소 요청 금액 중 면세 금액. 누락 시 0원으로 간주합니다.
+            reason (str): 취소 사유
+
+        Returns:
+            dict
+        """
+        api_instance = Payments(auth=self.imp_auth, session=self.requests_session)
+        response = api_instance.post_cancel(imp_uid=imp_uid, merchant_uid=merchant_uid,
+                                            amount=amount, tax_free=tax_free,
+                                            reason=reason, )
 
         if not response.is_succeed:
             raise ImpApiError(response)
