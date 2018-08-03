4dca5e5eb5ee2fe6dcf8f6d9b4fc8ca53bab53e9
kde713 <kde713@gmail.com>
2018-08-03 12:34:04 +0900

    버전 및 dependency 업데이트

--- iamporter/__init__.py
+++ iamporter/__init__.py
@@ -2,7 +2,7 @@ from .base import IamportResponse, IamportAuth
 from . import api, consts, errors
 from .client import Iamporter
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 __all__ = ['__version__',
            'IamportResponse', 'IamportAuth',
