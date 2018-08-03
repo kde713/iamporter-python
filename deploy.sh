fb323450249555b3b3de8f1a9e68df4476050c0d
kde713 <kde713@gmail.com>
2018-08-03 13:04:49 +0900

    배포 관련 스크립트 및 설정 추가

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+rm dist/*
+python setup.py bdist_wheel
+
+if [ $# -ge 1 ] && [ $1 == "test" ]; then
+    twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+else
+    twine upload dist/*
+fi
