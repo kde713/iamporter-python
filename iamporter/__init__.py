8aabfc92019941c5c34aa74cd292c9418a161795
kde713 <kde713@gmail.com>
2018-10-01 11:14:03 +0900

    Version Update (0.2.0)

--- iamporter/__init__.py
+++ iamporter/__init__.py
@@ -2,7 +2,7 @@ from .base import IamportResponse, IamportAuth
 from . import api, consts, errors
 from .client import Iamporter
 
-__version__ = "0.1.4"
+__version__ = "0.2.0"
 
 __all__ = ['__version__',
            'IamportResponse', 'IamportAuth',
