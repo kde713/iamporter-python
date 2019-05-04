a2c833238977c6cd69141298f2defb6eaaa23dcf
Dongeon Kim <kde713@gmail.com>
2019-05-04 20:41:44 +0900

    불필요한 dependency 명시 제거 및 requests 최소 버전 업데이트

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_packages(exclude=['test', 'tests', 'docs', 'examples']),
 
     install_requires=[
-        'requests~=2.20.0',
+        'requests~=2.21.0',
     ],
 
     python_requires='>=3',
