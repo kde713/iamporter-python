635b21b04fd83d8e74369b0d5b10b6b508cf6d63
Dongeon Kim <kde713@gmail.com>
2019-06-14 16:57:56 +0900

    커버리지를 저해하지 않도록 테스트 코드 수정

--- test.py
+++ test.py
@@ -152,7 +152,7 @@ class TestIamporter(unittest.TestCase):
     def test_create_payment_onetime(self):
         self.assertRaises(KeyError, self.client.create_payment, merchant_uid=None, name="테스트", amount=10000)
         self.assertRaises(KeyError, self.client.create_payment, merchant_uid='some-special-uid', name="테스트",
-                          card_number="1111-1111-1111-1111", expiry="2022-06")
+                          amount=10000, card_number="1111-1111-1111-1111")
 
         try:
             self.client.create_payment(merchant_uid='some-special-uid', amount=1000, name="테스트",
