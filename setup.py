3b03611779f98a4a30adde722ed875cc1da63fef
kde713 <kde713@gmail.com>
2020-12-26 17:42:11 +0900

    requests 버전 범위를 좀 더 넓게 지정

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     packages=find_packages(exclude=['test', 'tests', 'docs', 'examples']),
 
     install_requires=[
-        'requests~=2.21.0',
+        'requests>=2.0.0,<3.0.0',
     ],
 
     python_requires='>=3',
